62665bcb6c23a8d24bbd55e9bb9338be13f9a1ce
pasotti.matteo.98 <pasotti.matteo.98@outlook.it>
2023-03-22 23:18:55 +0100

    Sistemato WebScraping e raccolto i dati corettamente

--- Data Acquisition/WS_Season2022.py
+++ Data Acquisition/WS_Season2022.py
@@ -25,10 +25,10 @@ drivers = ['17', '22', '9', '13', '10', '15', '848', '3', '6', '7', '21', '11', 
 data = {}
 data_inside = ['Summary', 'Practice', 'Qualifying', 'Pitstops', 'Race progression', 'Laps']
 
-
-# Itera su tutte le possibili combinazioni di stagione, gara e pilota
-for s, r, d in itertools.product(seasons, races, drivers):
+# Itera su tutte le possibili combinazioni di pilota, gara e stagione
+for d, r, s in itertools.product(drivers, races, seasons):
     # Costruisce l'URL con i valori correnti
+
     current_url = f'https://pitwall.app/analysis/race-report?utf8=%E2%9C%93&season={s}&race={r}&driver={d}&button='
     # Visita l'URL con Selenium
     driver.get(current_url)
@@ -36,11 +36,11 @@ for s, r, d in itertools.product(seasons, races, drivers):
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".section")))
     # Crea un oggetto Beautiful Soup a partire dal contenuto HTML della pagina
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    Title = soup.select_one('.section')
-    pattern = r"(\d{4})\s+(\w+\s+\w+)\s+-\s+(\w+\s+\w+)"
-
+    titles = soup.select('.block-title')
+    Title = titles[1].getText().split()
+    pattern = r"(\d{4})\s+(.+)\s+-\s+(\w+\s+\w+)"
     # Esegui la ricerca del pattern nella stringa di esempio
-    match = re.search(pattern, Title.getText())
+    match = re.search(pattern, titles[1].getText())
 
     # Estrai le tre stringhe corrispondenti ai tre gruppi di cattura del pattern regex
     if match:
@@ -50,10 +50,41 @@ for s, r, d in itertools.product(seasons, races, drivers):
 
     # Estrae i dati dal contenuto della pagina
     sections = soup.select('.section')
-    data[f"{driver_name} - {race_name}"] = {}
-    for i, section in enumerate(sections):
+    data.setdefault(driver_name, {})
+    data[driver_name].setdefault(race_name, {})
+    for i, section in enumerate(sections[1:]):
+
         section_data = section.get_text().strip().replace('\n', '')
-        data[f"{driver_name} - {race_name}"][data_inside[i]] = section_data
+        for title in titles:
+            if title.getText() in section_data:
+                data[driver_name][race_name][title.getText()] = section_data
+                break
+    data[driver_name][race_name].pop('Summary', None)
+    data[driver_name][race_name].pop('Practice', None)
+    data[driver_name][race_name].pop('Pitstops', None)
+    data[driver_name][race_name].pop('Race progression', None)
+    if 'Qualifying' in data[driver_name][race_name]:
+        Qualifying_data = data[driver_name][race_name]['Qualifying']
+        if type(data[driver_name][race_name]['Qualifying']) == str:
+            data[driver_name][race_name]['Qualifying'] = {}
+            q3_time = re.findall(r'Q3 Time(\d+:\d+\.\d+)', Qualifying_data)
+            q2_time = re.findall(r'Q2 Time(\d+:\d+\.\d+)', Qualifying_data)
+            q1_time = re.findall(r'Q1 Time\s+(\d+:\d+\.\d+)', Qualifying_data)
+            data[driver_name][race_name]['Qualifying']['Q3 Time'] = q3_time
+            data[driver_name][race_name]['Qualifying']['Q2 Time'] = q2_time
+            data[driver_name][race_name]['Qualifying']['Q1 Time'] = q1_time
+    if 'Laps' in data[driver_name][race_name]:
+        Lap_data = data[driver_name][race_name]['Laps']
+        if type(data[driver_name][race_name]['Laps']) == str:
+            data[driver_name][race_name]['Laps'] = {}
+            # Definisci il pattern regex per estrarre i tempi
+            pattern = r"\d+:\d+\.\d+"
+            tempi = re.findall(pattern, str(Lap_data))
+            n_lap = range(1,len(tempi)+1)
+            for c in n_lap:
+                data[driver_name][race_name]['Laps'][c] = tempi[c-1]
+
+
     # Attende un breve periodo di tempo prima di effettuare la successiva richiesta
     time.sleep(1)
 
