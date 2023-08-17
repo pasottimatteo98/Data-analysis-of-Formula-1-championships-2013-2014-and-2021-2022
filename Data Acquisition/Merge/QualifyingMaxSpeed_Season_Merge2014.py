6744ea68092348350cbd97983514cb1c095625cb
Matteo Pasotti <pasotti.matteo98@gmail.com>
2023-08-17 21:44:44 +0200

    Bug Fix

--- Data Acquisition/Merge/QualifyingMaxSpeed_Season_Merge2014.py
+++ Data Acquisition/Merge/QualifyingMaxSpeed_Season_Merge2014.py
@@ -8,6 +8,13 @@ def preprocess_driver_name(name):
     # Assuming the name is in the format "Prima lettera nome.Cognome"
     return " ".join(name.split()[1:])
 
+def preprocess_driver_name2(name):
+    # Estrai l'ultima parte del nome dopo il punto
+    last_name = name.split('.')[-1]
+    # Converti la prima lettera in maiuscolo e il resto in minuscolo
+    formatted_name = last_name.capitalize()
+    return formatted_name
+
 # Path to the folder with JSON files
 folder_path = "../RawDataCollected/WebScraping/Qualifying_Max_Speed/2014"
 
@@ -28,7 +35,7 @@ for filename in os.listdir(folder_path):
 
         # Merge the two JSONs
         for driver_data in first_json:
-            driver = driver_data["driver"]
+            driver = preprocess_driver_name2(driver_data["driver"])
             gp = driver_data["gp"]
 
             # Get the last word from each key in the second JSON
@@ -37,7 +44,7 @@ for filename in os.listdir(folder_path):
             # Search for approximate matches of the driver's name in the second JSON
             driver_matches = process.extract(driver, second_json_keys_last_word, scorer=fuzz.token_sort_ratio, limit=3)
             for driver_match in driver_matches:
-                if driver_match[1] >= 70:
+                if driver_match[1] >= 85:
                     matched_driver_last_word = driver_match[0]
 
                     # Find the full key in the second JSON using the last word
@@ -47,7 +54,7 @@ for filename in os.listdir(folder_path):
                     gp_matches = process.extract(gp, second_json[matched_driver].keys(), scorer=fuzz.token_sort_ratio,
                                                  limit=3)
                     for gp_match in gp_matches:
-                        if gp_match[1] >= 99:
+                        if gp_match[1] >= 85:
                             matched_gp = gp_match[0]
                             # Insert the "km/h" data at the specific point
                             second_json[matched_driver][matched_gp]["km/h"] = driver_data["km/h"]
